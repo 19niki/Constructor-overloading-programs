aecc38cf1841da9d7854207a45c6c332f3af76a4
19niki <57766249+19niki@users.noreply.github.com>
2019-12-03 08:59:11 +0530

    Add files via upload
    Did this programs on 12/3/2019

---
+++ Laptopdriver.java
@@ -0,0 +1,12 @@
+class Laptopdriver 
+{
+	public static void main(String[] args) 
+	{
+		Laptop l1=new Laptop(4,50000);
+		Laptop l2=new Laptop(5,60000);
+		l1.laptopdetails();
+		l2.laptopdetails();
+
+
+	}
+}
